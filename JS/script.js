6769cf0bb52e2dc0c6c8a42f1836adc17fdb0211
NataliChSe <natali.serg.ch@gmail.com>
2024-02-16 11:04:27 +0300

    code revision JS

--- JS/script.js
+++ JS/script.js
@@ -10,85 +10,85 @@ alert(a);
 
 // ***   2   ***
 
-// const firstReleaseIphone = 2007;
-// alert(firstReleaseIphone);
+const firstReleaseIphone = 2007;
+alert(firstReleaseIphone);
 
 
 
 // ***   3   ***
 
-// const creatorNameJS  = 'Брендан Эйх';
-// alert(creatorNameJS);
+const creatorNameJS  = 'Брендан Эйх';
+alert(creatorNameJS);
 
 
 
 // ***   4   ***
 
-// let one = 10;
-// let two = 2;
-// alert(one+two);
-// alert(one-two);
-// alert(one*two);
-// alert(one/two);
+let one = 10;
+let two = 2;
+alert(one + two);
+alert(one - two);
+alert(one * two);
+alert(one / two);
 
 
 
 // ***   5   ***
 
-// let result = (2**5);
-// alert(result);
+let result = (2 ** 5);
+alert(result);
 
 
 
 // ***   6   ***
 
-// let a = 9;
-// let b = 2;
-// alert(a%b);
+let a = 9;
+let b = 2;
+alert(a % b);
 
 
 
 // ***   7   ***
 
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// num = ++num;
-// num = --num;
-// alert(num);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
 
 
 
 // ***   8   ***
 
-// let age = prompt('Сколько вам лет?');
-// alert(`Вам ${age} лет`);
+let age = prompt('Сколько вам лет?');
+alert(`Вам ${age} лет`);
 
 
 
 // ***   9   ***
 
-// const user = {
-//     name: 'Larisa',
-//     age: 23,
-//     isAdmin: true,
-// };
+const user = {
+    name: 'Larisa',
+    age: 23,
+    isAdmin: true,
+};
 
-// user['city of residence'] = 'Krasnoyarsk';
+user['city of residence'] = 'Krasnoyarsk';
 
-// user.age = 25;
+user.age = 25;
 
-// delete user['city of residence'];
+delete user['city of residence'];
 
-// let info = prompt('Какую информацию хотите узнать о пользователе?');
+let info = prompt('Какую информацию хотите узнать о пользователе?');
 
-// alert(user[info]);
+alert(user[info]);
 
 
 
 // ***   10   ***
 
-// let firstName = prompt('Как твое имя?');
-// alert(`Привет, ${firstName}!`);
+let firstName = prompt('Как твое имя?');
+alert(`Привет, ${firstName}!`);
